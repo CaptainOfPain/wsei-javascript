37ad5eafa8896fa74bb49c8fef63f574f82cd2dd
Święch Jakub <jakub.swiech@microsoft.wsei.edu.pl>
2019-11-24 10:42:04 +0100

    zad8notall

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
+    //zadanie 0
+    var titleElement = document.getElementsByClassName('title')[0];
+    var dataAnimation = getDataAnimation(titleElement);
+    console.log(dataAnimation);
 
+    //zadanie 1
+    //1
+    let firstId = document.getElementById('home');
+    let secondId = home;
+
+    console.log(firstId, secondId);
+    
+    //zadanie 2
+    let divs = document.getElementsByTagName('div');
+    let paragraphs = [];
+    for(let i = 0; i < divs.length; i++) {
+        let x = divs[i].querySelector('p').filter(x => x != null);
+        paragraphs.push(x);
+        // for(let j = 0; j < divs[i].childNodes.length; j++) {
+        //     if(divs[i].childNodes[j].tagName === 'P') {
+        //         paragraphs.push(divs[i].childNodes[j]);
+        //     }
+        // }
+    }
+
+    console.log(paragraphs);
 });
+
+function getDataAnimation(element) {
+    var dataAnimation = element.getAttribute('data-animation');
+    return dataAnimation;
+}
