c1cf3f57eefda036c570e9d1108d386ff70f1e85
Święch Jakub <jakub.swiech@microsoft.wsei.edu.pl>
2019-12-22 11:07:13 +0100

    add zad 9 and 10

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania
-     */
+    //zadanie 0
+    var elements = document.querySelectorAll('.ex5 ul li');
+    for(let index = 0; index < elements.length; index ++) {
+        if(index % 2 == 0) {
+            elements[index].style.backgroundColor = 'green';
+        }
+        if(index % 3 == 0) {
+            elements[index].style.textDecoration = "underline overline";
+        }
+    }
+    elements[4].classList.add("big");
+    //zadanie 2
+    document.getElementById('name').innerHTML = "Jakub Święch";
+    //zadanie 3
 
+    //zadanie 4
+    let list = document.querySelectorAll(".ex4 ul li");
+    for(let index = 0; index < list.length; index++) {
+        list[index].dataset.id = index+1;
+    }
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -8,14 +8,48 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania
      */
-    getDatasInfo(links);
+    //zad 0
+    console.log(getDatasInfo(links));
+    //zad 1
+    console.log(homeElement);
+    console.log(childElements);
+    for(let child of childElements){
+        console.log(child);
+    }
+    console.log(banner);
+    console.log(blocks);
+    for(let block of blocks){
+        console.log(block);
+    }
+    console.log(links);
+    for(let link of links){
+        console.log(link);
+    }
+    //zad 2
+    for(let block of blocks){
+        console.log(block.innerHTML);
+        console.log(block.outerHTML);
+
+        block.innerHTML = 'Nowa wartość diva o klasie blocks';
+        //innerHTML zwraca środek naszego elementu, a outer zwraca całość, razem z naszym elementem.
+    }
+    //zad 3 
+    let element = document.getElementById('mainFooter');
+    console.log(getId(element));
+    //zad 4
 
 });
 
 function getDatasInfo(elements) {
     let datas = [];
-    for(let element of elements) {
-        let data = element.getAttribute('data');
-        console.log(data);
+    for(let i = 0; i < elements.length; i++) {
+        let data = elements[i].getAttribute('data-color');
+        datas.push(data);
     }
+
+    return datas;
+}
+
+function getId(element){
+    return element.getAttribute('id');
 }
