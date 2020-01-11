a8c993a99fe29ae30b0304d84e20d4f5a195c7db
jakub.swiech@kotrak.com <jakub.swiech@kotrak.com>
2020-01-11 14:45:14 +0100

    Add zadanie 10

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -13,7 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
     //zadanie 2
     document.getElementById('name').innerHTML = "Jakub Święch";
     //zadanie 3
-
+    var ul = document.querySelector('.ex3 ul');
+    ul.classList.add('menu');
+    var lis = ul.querySelectorAll('li');
+    for(let i = 0; i < lis.length; i++) {
+        lis[i].classList.remove('error');
+        lis[i].classList.add('menuElement');
+    }
     //zadanie 4
     let list = document.querySelectorAll(".ex4 ul li");
     for(let index = 0; index < list.length; index++) {
